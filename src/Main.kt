88f9e00947057778fe93e1f5afbcb33550e38e4d
AanaGreg <ana.gregic0@gmail.com>
2024-12-01 19:01:58 +0100

    Add files via upload

---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val name1 = ("Ana")
+    val name2 = ("Dumbledore")
+    val name3 = ("Snape")
+    println("In dining hall $name1 is in the middle, $name3 is on her right, and $name2 on her left")
+}
